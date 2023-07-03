7be44a8914a1ce6a359f52071e79f640e8576737
oixc <jan.h.hofmann@googlemail.com>
2023-07-03 18:01:00 +0200

    bug fix: speed commands to the motors need to be positive

--- movement_control/motor.cpp
+++ movement_control/motor.cpp
@@ -13,12 +13,18 @@ void Motor::control(int16_t speed_A, int16_t speed_B) {
   digitalWrite(PIN_Motor_STBY, HIGH);
 
   bool direction_A = direction_forward;
-  if (speed_A < 0) { direction_A = direction_backward; }
+  if (speed_A < 0) {
+    direction_A = direction_backward;
+    speed_A *= -1;
+  }
   digitalWrite(PIN_Motor_AIN_1, direction_A);
   analogWrite(PIN_Motor_PWMA, speed_A);
 
   bool direction_B = direction_forward;
-  if (speed_B < 0) { direction_B = direction_backward; }
+  if (speed_B < 0) {
+    direction_B = direction_backward;
+    speed_B *= -1;
+  }
   digitalWrite(PIN_Motor_BIN_1, direction_B);
   analogWrite(PIN_Motor_PWMB, speed_B);
 }
